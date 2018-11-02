5942dd59e771b1874c2090b747072eb7adb37dc3
Lucas Mendes da Silva <44248971+Luzeraa@users.noreply.github.com>
2018-11-01 21:07:26 -0300

    Atualizado, ajustando scanner

--- AppFuncionario.java
+++ AppFuncionario.java
@@ -3,89 +3,81 @@ package loja;
 import java.util.Scanner;
 
 public class AppFuncionario {
-    
-    public static void main(String[] args) {
-    	
-    	Scanner input = new Scanner(System.in);
-    	Scanner ler = new Scanner(System.in);
 
-    	Funcionario funcionario = new Funcionario();
-        FuncionarioSenior funcionarioSenior = new FuncionarioSenior ();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Funcionario funcionario = new Funcionario();
+        //FuncionarioSenior funcionarioSenior = new FuncionarioSenior();
         Gerencia gerencia = new Gerencia();
-    
-       
-        
+
         String nome, opcaoSenior;
-        double valorPorHora, bonus;  
+        double valorPorHora, bonus;
         int horasTrabalhadas;
         int opcao = 0;
-        
-        
-        
+
         do {
-        	
-        	
-        	System.out.println("1 - Cadastrar funcionario \n"
-        			+ "2 - Listar \n"
-        			+ "3 - Listar salários \n"
-        			+ "4 - Total de funcionarios senior \n"
-        			+ "5 - Funcionario com maior bonus \n"
-        			+ "6 - Listar por faixa salarial \n"
-        			+ "7 - Fechar");
-        	
-        	System.out.println("Digite a opcao");
-        	opcao = input.nextInt();
-        	
-        	
-        	switch(opcao) {
-        	
-        		case 1:
-        			System.out.println("Digite o nome do funcionario: " );
-        			nome = ler.nextLine();
-        			
-        			
-        			System.out.println("Digite o valor de horas trabalhadas: ");
-        			horasTrabalhadas = input.nextInt();
-        			
-        			System.out.println("Digite o valor por hora: " );
-        			valorPorHora=  input.nextDouble();
-        			
-        			System.out.println("O funcionario que voce ira cadastrar é Senior? \n"
-        					+ "Digite sim ou nao");
-        			opcaoSenior = ler.next();
-        			
-        			if(opcaoSenior.equalsIgnoreCase("sim")) {
-        				System.out.println("Digite o bonus do funcionario(se tiver) " );
-            			bonus = input.nextDouble();
-        			
-            			gerencia.cadastrarFuncionario(nome, horasTrabalhadas, valorPorHora, bonus);
-        			}
-        			
-        			gerencia.cadastrarFuncionario(nome, horasTrabalhadas, valorPorHora);
- 			
-        		break;
-        		
-        		case 2: 
-        			gerencia.listar();
-        		break;
-        		
-        		case 3:
-        			gerencia.listarSalario();
-        		break;
-        		
-        		case 4: 
-        			gerencia.totalDeFuncionarioSenior();
-        		break;
-        		
-        		case 5:
-        			gerencia.funcionarioComMaiorBonus();
-        		break;
-        			 		 
-        	}
-        	
-        	
-        }while(opcao <= 7);
-        
-     }
+            System.out.println("1 - Cadastrar funcionario \n"
+                    + "2 - Listar \n"
+                    + "3 - Listar salï¿½rios \n"
+                    + "4 - Total de funcionarios senior \n"
+                    + "5 - Funcionario com maior bonus \n"
+                    + "6 - Listar por faixa salarial \n"
+                    + "7 - Fechar");
+            
+            System.out.println("Digite a opcao");
+            opcao = input.nextInt();
+            
+
+            switch (opcao) {
+
+                case 1:
+                    System.out.println("Digite o nome do funcionario: ");
+                    nome = input.next();
+                    //nome = input.nextLine().ne;
+                    
+                    System.out.println("Nome digitado:"+nome);
+
+                    System.out.println("Digite o valor de horas trabalhadas: ");
+                    horasTrabalhadas = input.nextInt();
+
+                    System.out.println("Digite o valor por hora: ");
+                    valorPorHora = input.nextDouble();
+
+                    System.out.println("O funcionario que voce ira cadastrar ï¿½ Senior? \n"
+                                     + "Digite sim ou nao");
+                    opcaoSenior = input.next();
+
+                    if (opcaoSenior.equalsIgnoreCase("sim")) {
+                        System.out.println("Digite o bonus do funcionario(se tiver) ");
+                        bonus = input.nextDouble();
+                        gerencia.cadastrarFuncionario(nome, horasTrabalhadas, valorPorHora, bonus);
+                    }
+
+                    gerencia.cadastrarFuncionario(nome, horasTrabalhadas, valorPorHora);
+
+                    break;
+
+                case 2:
+                    gerencia.listar();
+                    break;
+
+                    
+                case 3:
+                    gerencia.listarSalario();
+                    break;
+
+                case 4:
+                    gerencia.totalDeFuncionarioSenior();
+                    break;
+
+                case 5:
+                    gerencia.funcionarioComMaiorBonus();
+                    break;
+
+            }
+
+        } while (opcao <= 7);
+
+    }
 
 }
--- Gerencia.java
+++ Gerencia.java
@@ -3,70 +3,61 @@ package loja;
 import java.util.ArrayList;
 
 public class Gerencia {
-	private ArrayList<Funcionario> lista;
-	
-	public Gerencia() {
-		lista = new ArrayList<>();
-	}
-	
-	
-	public void cadastrarFuncionario(String nome, int horasTrabalhadas, double valorPorHora, double bonus) {
-		FuncionarioSenior funcionario  = new FuncionarioSenior( nome,  horasTrabalhadas,  valorPorHora, bonus);
-		lista.add(funcionario);
-		
-	}
-	
-	public void cadastrarFuncionario(String nome, int horasTrabalhadas, double valorPorHora) {
-		Funcionario funcionario  = new Funcionario( nome,  horasTrabalhadas,  valorPorHora);
-		lista.add(funcionario);
-		
-	}
-	
-	public void listar() {
-		for (Funcionario funcionario : lista) {
-			if(funcionario instanceof FuncionarioSenior) {
-				System.out.println(funcionario.exibir());
-				}else {
-				System.out.println(funcionario.exibir());
-				}
-			
-			
-		}
-	}
-	
-	
-	public String listarSalario() {
-		String f="";
-		for (Funcionario funcionario : lista) {
-			return "Nome:" + funcionario.getNome() + "Salario: "+ funcionario.salarioFinal();
-		}
-		return f;
-	}
-	
-	public int totalDeFuncionarioSenior() {
-		int totalSenior = 0;
-		for (Funcionario funcionario : lista) {
-			if(funcionario instanceof FuncionarioSenior) {
-				return totalSenior++;
-			}
-		}
-		return totalSenior;
-	}
-	
-	
-	public String funcionarioComMaiorBonus() {
-		String f ="";
-		for (Funcionario funcionario : lista) {
-			
-			if(funcionario.salarioFinal() > funcionario.salarioFinal() ) {
-				return funcionario.getNome();			
-			}	
-		}
-		return f;
-	}
 
-	
-	
-	
+    private ArrayList<Funcionario> lista;
+
+    public Gerencia() {
+        lista = new ArrayList<>();
+    }
+
+    public void cadastrarFuncionario(String nome, int horasTrabalhadas, double valorPorHora, double bonus) {
+        FuncionarioSenior funcionario = new FuncionarioSenior(nome, horasTrabalhadas, valorPorHora, bonus);
+        lista.add(funcionario);
+    }
+
+    public void cadastrarFuncionario(String nome, int horasTrabalhadas, double valorPorHora) {
+        Funcionario funcionario = new Funcionario(nome, horasTrabalhadas, valorPorHora);
+        lista.add(funcionario);
+    }
+
+    public void listar() {
+        for (Funcionario funcionario : lista) {
+            if (funcionario instanceof FuncionarioSenior) {
+                System.out.println(funcionario.exibir());
+            } else {
+                System.out.println(funcionario.exibir());
+            }
+
+        }
+    }
+
+    public String listarSalario() {
+        String f = "";
+        for (Funcionario funcionario : lista) {
+            return "Nome:" + funcionario.getNome() + "Salario: " + funcionario.salarioFinal();
+        }
+        return f;
+    }
+
+    public int totalDeFuncionarioSenior() {
+        int totalSenior = 0;
+        for (Funcionario funcionario : lista) {
+            if (funcionario instanceof FuncionarioSenior) {
+                return totalSenior++;
+            }
+        }
+        return totalSenior;
+    }
+
+    public String funcionarioComMaiorBonus() {
+        String f = "";
+        for (Funcionario funcionario : lista) {
+
+            if (funcionario.salarioFinal() > funcionario.salarioFinal()) {
+                return funcionario.getNome();
+            }
+        }
+        return f;
+    }
+
 }
-
